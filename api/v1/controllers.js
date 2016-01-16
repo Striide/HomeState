c0f15ac43ec8a544e8a319f3fc6781db2431b78b
Chad Lindstrom <chad.lindstrom@gmail.com>
2016-01-15 20:24:33 -0800

    code formatting

--- api/v1/controllers.js
+++ api/v1/controllers.js
@@ -1,29 +1,29 @@
 var models = require('../../models');
-var PersonProvider= new models.PersonProvider();
-var ActionProvider= new models.ActionProvider();
-var AutomatableProvider= new models.AutomatableProvider();
+var PersonProvider = new models.PersonProvider();
+var ActionProvider = new models.ActionProvider();
+var AutomatableProvider = new models.AutomatableProvider();
 
 // =======================================================
-var ActionController = function(){};
+var ActionController = function() {};
 
 /**
  * @param {http.ClientRequest} req the request object
  * @param {http.ClientResponse} req the response object
  */
 ActionController.prototype.getAll = function(req, res) {
-  res.writeHead(200, {
-    'Content-Type': 'application/json'
-  });
+    res.writeHead(200, {
+        'Content-Type': 'application/json'
+    });
 
-  ActionProvider.findAll(function(err, actions) {
-    if (err) {
-      res.end();
-      return console.error(err);
-    }
-    console.log(JSON.stringify(actions));
-    res.write(JSON.stringify(actions));
-    res.end();
-  });
+    ActionProvider.findAll(function(err, actions) {
+        if (err) {
+            res.end();
+            return console.error(err);
+        }
+        console.log(JSON.stringify(actions));
+        res.write(JSON.stringify(actions));
+        res.end();
+    });
 };
 
 /**
@@ -31,31 +31,31 @@ ActionController.prototype.getAll = function(req, res) {
  * @param {http.ClientResponse} req the response object
  */
 ActionController.prototype.createOne = function(req, res) {
-  ActionProvider.create({
-    name: req.body.name,
-    action: req.body.action
-  },function(err) {
-    if (err) {
-      res.writeHead(500, {
-        'Content-Type': 'application/json'
-      });
-      res.end();
-      return console.err(err);
-    }
-    res.writeHead(200, {
-      'Content-Type': 'application/json'
+    ActionProvider.create({
+        name: req.body.name,
+        action: req.body.action
+    }, function(err) {
+        if (err) {
+            res.writeHead(500, {
+                'Content-Type': 'application/json'
+            });
+            res.end();
+            return console.err(err);
+        }
+        res.writeHead(200, {
+            'Content-Type': 'application/json'
+        });
+        res.write(JSON.stringify({
+            'success': 1
+        }));
+        res.end();
+        console.log(err);
     });
-    res.write(JSON.stringify({
-      'success': 1
-    }));
-    res.end();
-    console.log(err);
-  });
 };
 exports.ActionController = ActionController;
 
 // =======================================================
-var PersonController = function(){};
+var PersonController = function() {};
 
 /**
  * @param {http.ClientRequest} req the request object
@@ -63,68 +63,70 @@ var PersonController = function(){};
  */
 PersonController.prototype.getAll = function(req, res) {
     res.writeHead(200, {
-    'Content-Type': 'application/json'
-  });
+        'Content-Type': 'application/json'
+    });
 
     PersonProvider.findAll(function(err, people) {
-      if (err) {
-        return console.error(err);
-      }
-      console.log(JSON.stringify(people));
-      res.write(JSON.stringify(people));
-      res.end();
+        if (err) {
+            return console.error(err);
+        }
+        console.log(JSON.stringify(people));
+        res.write(JSON.stringify(people));
+        res.end();
     });
-  };
-  
+};
+
 /**
  * @param {http.ClientRequest} req the request object
  * @param {http.ClientResponse} req the response object
  */
 PersonController.prototype.createOne = function(req, res) {
-    PersonProvider.create({name: req.body.name },function(err) {
-    if (err) {
-      res.writeHead(500, {
-        'Content-Type': 'application/json'
-      });
-      res.end();
-      return console.err(err);
-    }
-    res.writeHead(200, {
-      'Content-Type': 'application/json'
+    PersonProvider.create({
+        name: req.body.name
+    }, function(err) {
+        if (err) {
+            res.writeHead(500, {
+                'Content-Type': 'application/json'
+            });
+            res.end();
+            return console.err(err);
+        }
+        res.writeHead(200, {
+            'Content-Type': 'application/json'
+        });
+        res.write(JSON.stringify({
+            'success': 1
+        }));
+        res.end();
+        console.log(err);
     });
-    res.write(JSON.stringify({
-      'success': 1
-    }));
-    res.end();
-    console.log(err);
-  });
-  };
+};
 exports.PersonController = PersonController;
 
 
 // =======================================================
-var AutomatableController = function(){};
+var AutomatableController = function() {};
 
 /**
  * @param {http.ClientRequest} req the request object
  * @param {http.ClientResponse} req the response object
  */
 AutomatableController.prototype.getAll = function(req, res) {
-  res.writeHead(200, {
-    'Content-Type': 'application/json'
-  });
-  AutomatableProvider.findAll(function(err, automatable) {
-    if (err) {
-      res.writeHead(500, {
+    res.writeHead(200, {
         'Content-Type': 'application/json'
-      });
-      res.end();
-      return console.error(err);
-    }
-    console.log(JSON.stringify(automatable));
-    res.write(JSON.stringify(automatable));
-    res.end();
-  });
+    });
+    AutomatableProvider.findAll(function(err, automatable) {
+        if (err) {
+            res.writeHead(500, {
+                'Content-Type': 'application/json'
+            });
+            res.end();
+            return console.error(err);
+        }
+        console.log(JSON.stringify(automatable));
+        res.write(JSON.stringify(automatable));
+        res.end();
+    });
 };
 
 /**
@@ -132,25 +134,25 @@ AutomatableController.prototype.getAll = function(req, res) {
  * @param {http.ClientResponse} req the response object
  */
 AutomatableController.prototype.createOne = function(req, res) {
-  AutomatableProvider.create({
-    name: req.body.name,
-    state: req.body.state
-  },function(err) {
-    if (err) {
-      res.writeHead(500, {
-        'Content-Type': 'application/json'
-      });
-      res.end();
-      return console.err(err);
-    }
-    res.writeHead(200, {
-      'Content-Type': 'application/json'
+    AutomatableProvider.create({
+        name: req.body.name,
+        state: req.body.state
+    }, function(err) {
+        if (err) {
+            res.writeHead(500, {
+                'Content-Type': 'application/json'
+            });
+            res.end();
+            return console.err(err);
+        }
+        res.writeHead(200, {
+            'Content-Type': 'application/json'
+        });
+        res.write(JSON.stringify({
+            'success': 1
+        }));
+        res.end();
+        console.log(err);
     });
-    res.write(JSON.stringify({
-      'success': 1
-    }));
-    res.end();
-    console.log(err);
-  });
 };
 exports.AutomatableController = AutomatableController;
--- models.js
+++ models.js
@@ -14,8 +14,9 @@ var Schema = mongoose.Schema;
 
 // -------------- SCHEMA -----------
 var personSchema = mongoose.Schema({
-  name: String,
-  versionKey: false 
+  name: String
+}, {
+  versionKey: false
 });
 personSchema.methods.speak = function() {
   var greeting = this.name ? "Hi name is " + this.name : "I don't have a name";
@@ -24,8 +25,9 @@ personSchema.methods.speak = function() {
 
 var automatableSchema = mongoose.Schema({
   name: String,
-  state: String,
-  versionKey: false 
+  state: String
+}, {
+  versionKey: false
 });
 automatableSchema.methods.automate = function() {
   console.log(this.name + " is " + this.state);
@@ -35,8 +37,12 @@ automatableSchema.methods.automate = function() {
 var actionSchema = mongoose.Schema({
   name: String,
   action: String,
-  action_time: {type: Date, default: Date.now},
-  versionKey: false 
+  action_time: {
+    type: Date,
+    default: Date.now
+  }
+}, {
+  versionKey: false
 });
 actionSchema.methods.finish = function() {
   var log = this.name ? "Attention: " + this.name + "has performed the following action: " + this.action : "Hmm... not sure what just happened";
@@ -48,15 +54,15 @@ var Action = mongoose.model('Action', actionSchema);
 var Automatable = mongoose.model('Automatable', automatableSchema);
 
 // ===============================================================
-PersonProvider = function(){};
+PersonProvider = function() {};
 
 PersonProvider.prototype.findAll = function(callback) {
-  Person.find({}, function (err, people) {
-    callback( err, people )
-  }).lean().exec();  
+  Person.find({}, function(err, people) {
+    callback(err, people)
+  }).lean().exec();
 };
 
-PersonProvider.prototype.create = function(options,callback) {
+PersonProvider.prototype.create = function(options, callback) {
   var person = new Person(options);
   person.save(callback);
 };
@@ -65,15 +71,15 @@ exports.PersonProvider = PersonProvider;
 
 
 // ===============================================================
-ActionProvider = function(){};
+ActionProvider = function() {};
 
 ActionProvider.prototype.findAll = function(callback) {
-  Action.find({}, function (err, actions) {
-    callback( err, actions )
-  }).lean().exec();  
+  Action.find({}, function(err, actions) {
+    callback(err, actions)
+  }).lean().exec();
 };
 
-ActionProvider.prototype.create = function(options,callback) {
+ActionProvider.prototype.create = function(options, callback) {
   var action = new Action(options);
   action.save(callback);
 };
@@ -81,15 +87,15 @@ ActionProvider.prototype.create = function(options,callback) {
 exports.ActionProvider = ActionProvider;
 
 // ===============================================================
-AutomatableProvider = function(){};
+AutomatableProvider = function() {};
 
 AutomatableProvider.prototype.findAll = function(callback) {
-  Automatable.find({}, function (err, automatables) {
-    callback( err, automatables )
-  }).lean().exec();  
+  Automatable.find({}, function(err, automatables) {
+    callback(err, automatables)
+  }).lean().exec();
 };
 
-AutomatableProvider.prototype.create = function(options,callback) {
+AutomatableProvider.prototype.create = function(options, callback) {
   var automatable = new Automatable(options);
   automatable.save(callback);
 };
